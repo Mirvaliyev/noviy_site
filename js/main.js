9197c73ed359d696d6fc64c7f0096187031ccf37
Mirvaliyev <mirvaliyev2000@gamil.com>
2022-01-24 10:05:08 +0500

    ha

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-let in_audio=document.getElementById('in_audio');
-setTimeout(() => {
-    in_audio.play();
+let sa=document.getElementById('sa');
+setTimeout(function(){
+    if(sa.paused){
+        sa.play();
+    }
 }, 4000);
 
-let icon_audio=document.getElementById('icon_audio');
+
+let btn_audio=document.getElementById('btn_audio');
+let span_audio=document.querySelectorAll('.span_audio');
 let site_audio=document.getElementById('site_audio');
-icon_audio.onclick=function(){
+btn_audio.onclick=function(){
     if(site_audio.paused){
         site_audio.play();
-        icon_audio.name="pause-circle-outline";
+        for(let i=0;i<span_audio.length;i++){
+            span_audio[i].classList.toggle('span_toggle');
+        }
     }
     else{
         site_audio.pause();
-        icon_audio.name="play-circle-outline";
+        for(let j=0;j<span_audio.length;j++){
+            span_audio[j].classList.toggle('span_toggle');
+        }
     }
 }
